8113538ef90603fdab18e58a2e57da8b49f6c476
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-08-11 13:29:57 +0200

    Move constants to cpp file

--- include/image_processing_app.hpp
+++ include/image_processing_app.hpp
@@ -8,13 +8,6 @@
 
 #include "image_processing.hpp"
 
-constexpr std::size_t OFFSET_TO_GREEN{1};
-constexpr std::size_t OFFSET_TO_BLUE{2};
-constexpr std::size_t RGB_STEP{3};
-constexpr std::size_t INDEX_OF_INPUT_OPTION{1};
-constexpr std::size_t INDEX_OF_PATH_TO_FILE{2};
-constexpr int EXPECTED_NUMBER_OF_ARGUMENTS{3};
-
 namespace image_processing_app
 {
 image_processing::RgbImage createRGBImageFromVectorOfBytes(
